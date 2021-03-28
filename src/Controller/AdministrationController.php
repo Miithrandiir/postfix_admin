6c284f6da15681c1f654e7b3d33e6ee9cb83da37
miithrandiir <simon.heban@ferenost.fr>
2021-03-28 15:36:04 +0200

    feat: Added some translations

---
+++ src/Controller/AdministrationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdministrationController extends AbstractController
+{
+    /**
+     * @Route("/administration", name="administration")
+     */
+    public function index(): Response
+    {
+        return $this->render('administration/index.html.twig', [
+            'controller_name' => 'AdministrationController',
+        ]);
+    }
+}
