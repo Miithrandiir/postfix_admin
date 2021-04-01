f9c1f13c84ff9b2ba66062341c691290336a490b
miithrandiir <simon.heban@ferenost.fr>
2021-04-01 20:18:27 +0200

    style: change all design feat: add counter of users domains and mailboxes

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Domain;
+use App\Entity\Mailbox;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +16,18 @@ class AdministrationController extends AbstractController
      */
     public function index(): Response
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $nb_user = $em->getRepository(User::class)->count([]);
+        $nb_domains = $em->getRepository(Domain::class)->count([]);
+        $nb_mailboxes = $em->getRepository(Mailbox::class)->count([]);
+
         return $this->render('administration/index.html.twig', [
-            'controller_name' => 'AdministrationController',
+            'count' => [
+                'user' => $nb_user,
+                'domain' => $nb_domains,
+                'mailbox' => $nb_mailboxes
+            ]
         ]);
     }
 }
